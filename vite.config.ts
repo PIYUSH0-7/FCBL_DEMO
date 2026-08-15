f7281145b6ff4e95aa38f02c310edb3e95211cee
PIYUSH0-7 <gangwarpiyush1247@gmail.com>
2026-08-15 14:45:25 +0530

    feat: add @vercel/remix preset for native Vercel and Hydrogen multi-cloud deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import {defineConfig} from 'vite';
 import {hydrogen} from '@shopify/hydrogen/vite';
 import {vitePlugin as remix} from '@remix-run/dev';
+import {vercelPreset} from '@vercel/remix/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     hydrogen(),
     remix({
-      presets: [hydrogen.preset()],
+      presets: [hydrogen.preset(), vercelPreset()],
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
